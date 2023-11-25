7ae3a5854aba19426367185598ba2dd4eea6d3ba
Kboateng346 <Kboateng346@gmail.com>
2023-11-25 01:37:42 +0000

    init commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file in /tmp
+
+file {'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
