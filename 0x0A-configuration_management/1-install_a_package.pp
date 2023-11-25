feda73c39245e13f4bf30d400757c22eccaaba55
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-11-25 02:36:48 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask from pip3
-
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
