b9b83281f4d3262dde434ae616a650358de74f7b
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-11-25 02:11:09 +0000

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# Creates a file in /tmp
+# creates a file in /tmp
 
-file {'school':
-  ensure  => file,
-  path    => '/tmp/school',
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
