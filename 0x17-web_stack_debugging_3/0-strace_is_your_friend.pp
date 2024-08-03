1e0552bba02c0218c29fe3337336b869edafaeed
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2024-08-03 10:42:55 +0000

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Webstack debugging #3
-exec { 'Fix Wordpress':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
