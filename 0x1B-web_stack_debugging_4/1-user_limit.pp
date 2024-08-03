5a6488dd2106aefd2ab9ea8c318e1c8842714e63
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2024-08-03 10:43:19 +0000

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,5 +0,0 @@
-# holberton user to login and open a file without any error message
-
-exec { 'increase hard file limit':
-  command => '/usr/bin/env sed -i "s/4/30000/; s/5/30000/" /etc/security/limits.conf'
-}
