d6629eed552b0e0b67e6b0f22ad554a24155247b
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2024-08-14 02:14:45 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
