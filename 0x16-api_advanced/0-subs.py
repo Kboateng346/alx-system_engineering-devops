df01af9f2d4a5fec36d1d33533c484c96bcd4745
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2024-08-14 02:16:37 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
