defb8e6f7db7904ff904e6c03b91bbf94eb1be8a
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2024-08-20 23:12:08 +0000

    init commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fixes an nginx site that can't handle multiple concurrent requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
